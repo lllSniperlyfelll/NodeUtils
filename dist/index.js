3537e11f413f937ba6b31774a877e942a2da3a4d
lllSniperlyfelll <pravleen63@gmail.com>
2021-12-07 23:11:49 +0530

    Minor fixes

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.isStrEmpty = exports.capatalizeStr = void 0;
+const StrCapatalize_1 = require("./StrCapatalize");
+const StrEmpty_1 = require("./StrEmpty");
+function capatalizeStr(stringToCapatalize, toKebabCase = false) {
+    return (0, StrCapatalize_1.capatalizeStr)(stringToCapatalize, toKebabCase);
+}
+exports.capatalizeStr = capatalizeStr;
+function isStrEmpty(checkString) {
+    return (0, StrEmpty_1.isStrEmpty)(checkString);
+}
+exports.isStrEmpty = isStrEmpty;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import { capatalizeStr } from "./StrCapatalize";
-import { isStrEmpty } from "./StrEmpty";
+import { capatalizeStr as cpStr } from "./StrCapatalize";
+import { isStrEmpty as isStrEpty } from "./StrEmpty";
 
+export function capatalizeStr(
+  stringToCapatalize: string,
+  toKebabCase: boolean = false
+): string {
+  return cpStr(stringToCapatalize, toKebabCase);
+}
 
-
-
-
+export function isStrEmpty(checkString: string): boolean {
+    return isStrEpty(checkString);
+}
