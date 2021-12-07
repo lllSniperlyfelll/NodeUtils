71e043ee3b2148c64cbf83b2d2358a73d2864192
lllSniperlyfelll <pravleen63@gmail.com>
2021-12-07 23:05:29 +0530

    Build

---
+++ dist/StrEmpty.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.isStrEmpty = void 0;
+function isStrEmpty(checkString) {
+    if (checkString) {
+        const trimmedString = checkString.trim();
+        return trimmedString.length <= 0;
+    }
+    return true;
+}
+exports.isStrEmpty = isStrEmpty;
+//# sourceMappingURL=StrEmpty.js.map
